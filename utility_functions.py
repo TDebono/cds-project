47a8784c9007f8c0dff40ece03a52b26047e0ca5
Timo Debono <timo.debono@gmail.com>
2022-03-02 20:34:40 +0100

    add tutorials, corr analysis

---
+++ utility_functions.py
@@ -0,0 +1,14 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def magnify():
+    return [dict(selector="th",
+                 props=[("font-size", "7pt")]),
+            dict(selector="td",
+                 props=[('padding', "0em 0em")]),
+            dict(selector="th:hover",
+                 props=[("font-size", "12pt")]),
+            dict(selector="tr:hover td:hover",
+                 props=[('max-width', '200px'),
+                        ('font-size', '12pt')])
+]
